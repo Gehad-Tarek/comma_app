f297165f1bfb8de3b186bc56660bd1a3cad5ae45
jobmohhosny <145661895+jobmohhosny@users.noreply.github.com>
2023-10-04 14:02:01 +0300

    finsh report screen and stru folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
-// <<<<<<< HEAD
-
-// import 'package:comma_app/screens/home_screen/home_screen.dart';
-// import 'package:comma_app/screens/login_screen/login_screen.dart';
-// =======
-// // import 'package:comma_app/home_screen.dart';
-// import 'package:comma_app/screens/settings_screen/settings_screen.dart';
-// import 'package:comma_app/screens/tasks_screen/tasks_screen.dart';
-// import 'package:comma_app/screens/users_screen/users_screen.dart';
-// >>>>>>> 46cbd4d9a736d40b4b78d3372cf4497cab9df1ce
-// import 'package:flutter/material.dart';
-// import 'package:device_preview/device_preview.dart';
-
+import 'package:comma_app/screens/Reports_screen/Reports_screen.dart';
+import 'package:comma_app/screens/Tasks_screen/Tasks_screen.dart';
+import 'package:comma_app/screens/Users_screen/Users_screen.dart';
 import 'package:comma_app/screens/login_screen/login_screen.dart';
+import 'package:comma_app/screens/settings_screen/settings_screen.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
@@ -33,14 +24,16 @@ class MyApp extends StatelessWidget {
               colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
               useMaterial3: true,
             ),
-//<<<<<<< HEAD
+
             //home:const HomeScreen(),
-            home:const LoginScreen(),
-//=======
-            // home: SettingsScreen(),
-            // home: UsersScreen(),
-           // home: TasksScreen(),
-//>>>>>>> 46cbd4d9a736d40b4b78d3372cf4497cab9df1ce
+           // home:const LoginScreen(),
+          //  home:const ReportsScreen() ,
+          
+            // home:const SettingsScreen(),
+           //  home:const UsersScreen(),
+           // home:const TasksScreen(),
+          
+
           );
         });
   }
