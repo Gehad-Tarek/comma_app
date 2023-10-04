374d5474a35429b860e54f3d0618de91cfcd388f
Gehad Tarek <gehadtarek100@gmail.com>
2023-10-04 13:23:55 +0300

    Add Settings Screen and add change language option in it

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:comma_app/home_screen.dart';
+// import 'package:comma_app/home_screen.dart';
+import 'package:comma_app/screens/settings_screen/settings_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
               colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
               useMaterial3: true,
             ),
-            home: HomeScreen(),
+            home: SettingsScreen(),
           );
         });
   }
---
+++ lib/shared_widgets/settings_button.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class SettingsButton extends StatelessWidget {
+  String name;
+  IconData icon1;
+  IconData icon2;
+  Function() onPressed;
+
+  SettingsButton(this.icon1, this.icon2, this.name, this.onPressed, {Key? key})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .08,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        children: [
+          InkWell(
+            onTap: onPressed,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      icon1,
+                      color: Colors.blue[600],
+                      size: 22,
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * .035,
+                    ),
+                    Text(
+                      name,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+
+                  ],
+                ),
+                IconButton(
+                  onPressed: () {},
+
+                  icon:  Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.blue[600],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: MediaQuery.of(context).size.width * .1,
+            ),
+          ),
+          Divider(
+            height: 10,
+            color: Colors.grey[400],
+          ),
+        ],
+      ),
+    );
+  }
+}
