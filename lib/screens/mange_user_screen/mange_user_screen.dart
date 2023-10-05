d8732c5c584dc766c36279300be60da133606cbd
jobmohhosny <145661895+jobmohhosny@users.noreply.github.com>
2023-10-05 13:46:59 +0300

    edit appbar ui user screen

---
+++ lib/screens/mange_user_screen/mange_user_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MangeUserScreen extends StatelessWidget {
+   static const routeName = "/mangeUser";
+  const MangeUserScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
